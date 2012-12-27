46c5711b0d6a3016f6e652c498d4ceb3a9c2ddab
NaturalDre <naturaldre@gmail.com>
2012-12-27 18:18:55 -0500

    Added comments/useful mutators/RowCol struct.
    Added RowCol to represent a piece on the board and whether or not that
    is a 'valid'. Right now valid just offers a way for the code to know if
    this position is "good" or "bad". e.g. I mark m_selected's RowCol as
    false whenever the current player changes to signify that it is not the
    position the new player selected.

--- Cuby/CheckersBoard.cpp
+++ Cuby/CheckersBoard.cpp
@@ -1,13 +1,19 @@
 #include "CheckersBoard.h"
+#include "Debug.h"
+#include <list>
 
 // In Checkers, each player starts with pieces on the black squares of the first 3 rows on their side.
-static const size_t ROWS_PER_PLAYER = 3;
+const size_t ROWS_PER_PLAYER = 3;
+
 
 CCheckersBoard::CCheckersBoard(CEngine* engine)
 	: IGameBoard(engine)
 	, m_board()
+	, m_curPlayer(0)
+	, m_selected(0,0, false)
 {
-
+	SetPieceWidth(64);
+	SetPieceHeight(64);
 }
 
 CCheckersBoard::~CCheckersBoard(void)
@@ -17,18 +23,22 @@ CCheckersBoard::~CCheckersBoard(void)
 
 void CCheckersBoard::Start(void)
 {
-
+	IGameObject::Start();
 }
 
 void CCheckersBoard::Update(double dt)
 {
-
+	IGameObject::Update(dt);
 }
 
 bool CCheckersBoard::OnMatchStarted(void)
 {
+	NOTE("Attemping to start match...");
 	ClearBoard();
 	PlacePieces();
+
+	// Alpha always goes first.
+	m_curPlayer = ALPHA;
 	return false;
 }
 
@@ -39,12 +49,15 @@ bool CCheckersBoard::OnMatchEnded(void)
 
 void CCheckersBoard::ClearBoard(void)
 {
+	NOTE("Clearing board...");
 	m_board.fill(std::array<size_t, 8>());
+	NOTE("Board cleared.");
 }
 
 // TODO: Improve this function.
 void CCheckersBoard::PlacePieces(void)
 {
+	NOTE("Placing pieces...");
 	// Note: I assume 0 to be even.
 	bool isOdd = false;
 	// First top 3 rows
@@ -68,4 +81,105 @@ void CCheckersBoard::PlacePieces(void)
 			m_board[row][col] = BETA;
 		isOdd = !isOdd;
 	}
+
+	NOTE("Pieces have been placed.");
+}
+
+bool CCheckersBoard::Move(size_t row, size_t col)
+{
+	if (!GetSelected().valid)
+		return false;
+
+	if (IsValidMove(GetSelected(), RowCol(row,col, true)))
+	{
+		m_board[row][col] = GetOwner(GetSelected());
+		m_board[GetSelected().row][GetSelected().col] = BLANK;
+		m_selected.valid = false;
+		if (m_curPlayer == ALPHA)
+			m_curPlayer = BETA;
+		else
+			m_curPlayer = ALPHA;
+	}
+	return false;
+}
+
+bool CCheckersBoard::Select(size_t row, size_t col)
+{
+	const size_t OWNER = GetOwner(row, col);
+	// It has to be the player's turn and the spot must
+	// not be blank in order to select it.
+	if (m_curPlayer != OWNER || OWNER == BLANK)
+		return false;
+
+	m_selected = RowCol(row, col, true);
+	NOTE("Selected a piece.");
+	return true;
+}
+
+size_t CCheckersBoard::GetOwner(size_t row, size_t col) const
+{
+	// Make sure the location is in bounds.
+	if (row >= m_board.size() || col >= m_board[row].size())
+		return BLANK;
+	else
+		return m_board[row][col];
+}
+
+size_t CCheckersBoard::GetPlayerOwner(const RowCol& rc)
+{
+	if (GetOwner(rc) == ALPHA || GetOwner(rc) == KING_ALPHA) 
+		return ALPHA;
+	else if (GetOwner(rc) == BETA || GetOwner(rc) == KING_BETA)
+		return BETA;
+	return BLANK;
+}
+
+bool CCheckersBoard::IsKing(const RowCol& rc)
+{
+	const size_t TYPE = GetOwner(rc);
+	if (TYPE == KING_ALPHA || TYPE == KING_BETA)
+		return true;
+	return false;
+}
+
+bool CCheckersBoard::IsValid(const RowCol& rc)
+{
+	// warning: signed/unsigned mismatch
+	if (rc.row < GetBoard().size() || rc.col < GetBoard()[rc.col].size())
+		return true;
+	return false;
+}
+
+bool CCheckersBoard::IsValidMove(const RowCol& src, const RowCol& drc)
+{
+	// Return false if src and drc are not valid positions, 
+	// src is blank
+	// or src and drc are owned by the same player.
+	if (!IsValid(src) || !IsValid(drc) || GetPlayerOwner(src) == BLANK /*|| GetPlayerOwner(drc) == BLANK*/ || GetPlayerOwner(src) == GetPlayerOwner(drc))
+		return false;
+
+	const bool srcIsKing = IsKing(src);
+
+	if (srcIsKing)
+	{
+		if (std::abs((src.row - drc.row)) == 1 && std::abs(src.row - drc.col) == 1)
+			return true;
+	}
+	else
+	{
+		int owner = GetPlayerOwner(src);
+		int a = ALPHA;
+		if (GetPlayerOwner(src) == ALPHA)
+		{
+			if (src.row - drc.row == -1 && std::abs(src.col - drc.col) == 1)
+				return true;
+		}
+		else
+		{
+			if (src.row - drc.row == 1 && std::abs(src.col - drc.col) == 1)
+				return true;
+		}
+	}
+
+	return false;
 }
--- Cuby/CheckersBoard.h
+++ Cuby/CheckersBoard.h
@@ -4,12 +4,23 @@
 #include "GameBoard.h"
 #include <array>
 
+
+struct RowCol
+{
+	int row;
+	int col;
+	bool valid;
+	RowCol(size_t r = 0, size_t c = 0, bool v = true) : row(r), col(c), valid(v) { }
+};
+
 class CCheckersPiece;
 class CCheckersBoard: public IGameBoard
 {
 public:
 	static const size_t BLANK = 0;
+	/// Note: I refer to Player1 as ALPHA
 	static const size_t ALPHA = 1;
+	/// Note: I refer to Player2 as BETA
 	static const size_t BETA = 2;
 	static const size_t KING_ALPHA = 3;
 	static const size_t KING_BETA = 4;
@@ -18,12 +29,30 @@ public:
 	~CCheckersBoard(void);
 
 	bool Move(size_t row, size_t col);
-
+	bool Select(size_t row, size_t col);
+	inline size_t GetCurPlayer(void) const { return m_curPlayer; }
+	size_t GetOwner(size_t row, size_t col) const;
+	/// Returns what piece occupies a position.
+	/// @param rc The position you want to check.
+	/// \return The piece that occupies the position. (BLANK, ALPHA, BETA, KING_ALPHA, KING_BETA)
+	inline size_t GetOwner(const RowCol& rc) const { return GetOwner(rc.row, rc.col); }
+	/// Returns what *PLAYER* owns the piece that occupies this spot.
+	/// @param rc The position you want to check.
+	/// \return Either ALPHA, BETA, or BLANK. Remember, ALPHA is Player1, BETA is Player2.
+	size_t GetPlayerOwner(const RowCol& rc);
+	inline RowCol GetSelected(void) const { return m_selected; }
+	bool IsKing(const RowCol& rc);
+	bool IsValid(const RowCol& rc);
+	const std::array<std::array<size_t, 8>, 8>& GetBoard(void) const { return m_board; }
 protected:
 	void Start(void);
 	void Update(double dt);
 
+	/// Called when the user wants to start a new game.
+	/// \return Whether or not it successfully started.
 	bool OnMatchStarted(void);
+	/// Called when the use wants to end the current game.
+	/// \return Whether or not it successfully ended.
 	bool OnMatchEnded(void);
 
 	/// Removes every gamepiece on the board.
@@ -31,8 +60,16 @@ protected:
 	/// Add each gamepiece for both players to the board.
 	/// @note This should only be called by St
 	void PlacePieces(void);
+
+	/// Can this move be made?
+	/// @param src The position of the selected piece.
+	/// @param drc The position you want to move to.
+	/// \return Whether or not the move is valid.
+	bool IsValidMove(const RowCol& src, const RowCol& drc);
 private:
 	std::array<std::array<size_t, 8>, 8> m_board;
+	size_t m_curPlayer;
+	RowCol m_selected;
 };
 
 #endif
